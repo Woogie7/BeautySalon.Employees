cd8ae3aec38cb7f8b73e2dcf762cf7a155a0b626
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-03-25 21:50:34 +0300

    Емае

--- BeautySalon.Employees.Api/Program.cs
+++ BeautySalon.Employees.Api/Program.cs
@@ -35,7 +35,7 @@ app.MapPost("/employees", async (CreateEmployeeRequest createEmployeeRequest, [F
 
 app.MapGet("/employees", () =>
 {
-    return Results.Ok(new { message = "ÅrÏğèâûôâsåò worldÛ?" });
+    return Results.Ok(new { message = "lAST ïîÏÛÒÊÀ?" });
 });
 
 
