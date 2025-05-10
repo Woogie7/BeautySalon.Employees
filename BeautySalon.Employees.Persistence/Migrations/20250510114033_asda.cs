2ffff47f318c46c6c1e185eaedd34b65217c7192
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-10 15:21:17 +0300

    ееее

---
+++ BeautySalon.Employees.Persistence/Migrations/20250510114033_asda.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeautySalon.Employees.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class asda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_CustomDateOfWeek_DateOfWeekId",
+                table: "Schedules");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CustomDateOfWeek",
+                table: "CustomDateOfWeek");
+
+            migrationBuilder.RenameTable(
+                name: "CustomDateOfWeek",
+                newName: "CustomDateOfWeeks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Skills",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "EmployeeStatus",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Employees",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CustomDateOfWeeks",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CustomDateOfWeeks",
+                table: "CustomDateOfWeeks",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_CustomDateOfWeeks_DateOfWeekId",
+                table: "Schedules",
+                column: "DateOfWeekId",
+                principalTable: "CustomDateOfWeeks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_CustomDateOfWeeks_DateOfWeekId",
+                table: "Schedules");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CustomDateOfWeeks",
+                table: "CustomDateOfWeeks");
+
+            migrationBuilder.RenameTable(
+                name: "CustomDateOfWeeks",
+                newName: "CustomDateOfWeek");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Skills",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "EmployeeStatus",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CustomDateOfWeek",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CustomDateOfWeek",
+                table: "CustomDateOfWeek",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_CustomDateOfWeek_DateOfWeekId",
+                table: "Schedules",
+                column: "DateOfWeekId",
+                principalTable: "CustomDateOfWeek",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ BeautySalon.Employees.Persistence/Migrations/20250510114945_updateseek.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeautySalon.Employees.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateseek : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_CustomDateOfWeeks_DateOfWeekId",
+                table: "Schedules");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Employees",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_CustomDateOfWeeks_DateOfWeekId",
+                table: "Schedules",
+                column: "DateOfWeekId",
+                principalTable: "CustomDateOfWeeks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedules_CustomDateOfWeeks_DateOfWeekId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Employees");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedules_CustomDateOfWeeks_DateOfWeekId",
+                table: "Schedules",
+                column: "DateOfWeekId",
+                principalTable: "CustomDateOfWeeks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
