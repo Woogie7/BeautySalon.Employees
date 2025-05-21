8320c219d8e1df3d39308038991f8872ba6b3493
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-22 00:11:47 +0300

    ндэ

---
+++ BeautySalon.Employees.Persistence/Migrations/20250521181237_AddEnumData.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BeautySalon.Employees.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnumData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CustomDateOfWeeks",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Monday" },
+                    { 2, "Tuesday" },
+                    { 3, "Wednesday" },
+                    { 4, "Thursday" },
+                    { 5, "Friday" },
+                    { 6, "Saturday" },
+                    { 7, "Sunday" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CustomDateOfWeeks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CustomDateOfWeeks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CustomDateOfWeeks",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CustomDateOfWeeks",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "CustomDateOfWeeks",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "CustomDateOfWeeks",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "CustomDateOfWeeks",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
