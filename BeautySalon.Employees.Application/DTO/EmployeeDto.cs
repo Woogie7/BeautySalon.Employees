71b2321a44b953175e82f0674d3db2b11d06fc5a
Кирилл Цынгеев <ktsyngeev@bk.ru>
2025-05-22 02:47:25 +0300

    ещё

--- BeautySalon.Employees.Application/DTO/EmployeeDto.cs
+++ BeautySalon.Employees.Application/DTO/EmployeeDto.cs
@@ -4,6 +4,8 @@ public class EmployeeDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
     public List<ServiceDto> Services { get; set; }
     public List<ScheduleDto> Schedules { get; set; }
 }
